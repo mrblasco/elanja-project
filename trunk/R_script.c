50a7285f62ccb032381eae2a9945a3eecd59a065
zissex <zissex@05936896-c536-11de-ba9a-9db95b2bc6c5>
2009-11-12 18:43:53 +0000

    Tolto file inutile.

--- trunk/R_script.c
+++
@@ -1,9 +0,0 @@
-// non è (ancora) un file C ma serve per R
-
-library(sna);
-a <- scan("matrix.dat");
-dim(a) <- c(100,100);
-color <- c(rep(3,50), rep(2,50));
-png("nomefigura.png");
-gplot(a, vertex.col=color);
-dev.off();
