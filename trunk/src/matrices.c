3ac2f901300447cb44d63de15d97e33f1ee37544
mrblasco <mrblasco@05936896-c536-11de-ba9a-9db95b2bc6c5>
2009-12-01 18:07:26 +0000

    Ho fatto un po di modifiche... su matrices

--- trunk/src/matrices.c
+++ trunk/src/matrices.c
@@ -42,29 +42,26 @@ void interaction(double *p, double *q, int *A, double *R, int *degree, int *A_de
      	}
 
 	/* Adjacency Matrix */ 
-        for(i=0; i<ROW; i++)
-        {
+	for(i=0; i<ROW; i++)
+	{
 		for(j=i; j<COLUMN; j++)
 		{
-	   		if(j < M && i < M ) 
+			if(j < M && i < M ) 
 			{	
 				link = (double) *(R+(i*COLUMN)+j) - *(p+i) * *(p+j);
-				/*printf("Posizione %d %d = %f; dato da %f %f \n", i, j, link, *(p+i), *(p+j));*/
 			} else if (j >= M  && i < M ) 
 			{
-				link = (double) *(R+(i*COLUMN)+j) - *(q+i)* *(p+j);
-				/*printf("Posizione %d %d = %f; dato da %f %f \n", i, j, link, *(q+i), *(p+j));*/
+				link = (double) *(R+(i*COLUMN)+j) - *(q+i) * *(p+j);
 			} else if (j >= M && i >= M) 
 			{
 				link = (double) *(R+(i*COLUMN)+j) - *(q+i) * *(q+j);	
-				/*printf("Posizione %d %d = %f; dato da %f %f \n", i, j, link, *(q+i), *(q+j));*/
 			}
 			if (link < 0) 
 			{
 				*(A+(i*COLUMN)+j) = 1; 		 
 			}	 
 		}
-	  }
+	}
 	/* AM is symmmetric */
 	for(j=0; j<COLUMN; j++)
     	{
@@ -85,51 +82,55 @@ void interaction(double *p, double *q, int *A, double *R, int *degree, int *A_de
 	/* Degree vector*/ 
 	for(i=0; i<ROW; i++)
      	{
-		for(j=0; j<COLUMN; j++)
-		{
-			*(degree+i) += *(A+(i*COLUMN)+j);
-	   	}
+			for(j=0; j<COLUMN; j++)
+			{
+				*(degree+i) += *(A+(i*COLUMN)+j);
+		   	}
      	}
 
 	/* Degree of A-type agents vector*/ 
 	for(i=0; i<ROW; i++)
      	{
-		for(j=0; j<M; j++)
-		{
-			*(A_degree+i) += *(A+(i*COLUMN)+j);
-	   	}
-		for(j=M; j<COLUMN; j++)
-		{
-			*(B_degree+i) += *(A+(i*COLUMN)+j);
-	   	}
+			for(j=0; j<M; j++)
+			{
+				*(A_degree+i) += *(A+(i*COLUMN)+j);
+		   	}
+			for(j=M; j<COLUMN; j++)
+			{
+				*(B_degree+i) += *(A+(i*COLUMN)+j);
+		   	}
      	}
 }
 
 void update(double *p, double *q, int *degree, int *A_degree, int *B_degree){
 
 	int i;
-	
+	int control;	
+
 	/* Calculate the new p and q probabilities */
 	/* It is a linear combination of "relative degree" and connection composition*/
 	/* Relative degree: degree / total nodes number */
-
+	control = 0;
 	for(i=0; i<COLUMN; i++)
 	{	
-		if(*(degree+i) != 0)
-		{	
-			*(p+i) = 0.2 + 0.5 * (double) *(degree+i) / (double) COLUMN +  (double) *(A_degree+i) / (double) COLUMN;
-			*(q+i) = 0.2 + 0.5 * (double) *(degree+i) / (double) COLUMN +  (double) *(B_degree+i) / (double) COLUMN;
-			/*if (*(p+i)==0) 
-				*(p+i) = 0.5;
-			if (*(q+i)==0)		
-				*(q+i) = 0.5;*/			
-		}
-		else
+/*		if(*(degree+i) < COLUMN/10)
+		{	 
+		control++; */
+			*(p+i) = 0.1 + 0.4 * (double) *(A_degree + i) / *(degree + i); /*+ 0.5 * *(degree + i) / COLUMN;*/ 		
+			*(q+i) = 0.1 + 0.4 * (double) *(B_degree + i) / *(degree + i); /*+ 0.5 * *(degree + i)/ COLUMN;*/
+/*		}
+		else 
 		{
-			printf("Divided by zero!!!!\t");
-		}
+
+			*(p+i) *=  0.9;	
+			*(q+i) *=  0.9;
+			*(p+i) *=  (double) *(degree + i) / COLUMN;	
+			*(q+i) *=  (double) *(degree + i) / COLUMN;
+		}*/
 		
 	}	
+	/*printf("%d\n",control);	*/
+
 }
 
 void externalUpdate(int *A, double *p, double *q, double epsilon, int *degree){
@@ -144,22 +145,25 @@ void externalUpdate(int *A, double *p, double *q, double epsilon, int *degree){
 		if(*(degree+i) != 1)		
 		{
 			random = (double) (rand() %1000) / 1000;
-
-			if(random <= epsilon)
+			if(random < epsilon)
 			{
-				pos = rand() %(*(degree+i)-1);
-				tmp = 0;
+
+				pos = (rand() %(*(degree+i) -1));
+				tmp = 0; 
 				for (j=0; j<COLUMN; j++)
 				{
-					if(j != i)
-					{
-						if(*(A+(i*COLUMN)+j) == 1)
-							tmp++;
-						if(tmp == pos)
+					if (tmp != -1 ) /* -1 controllo che rompe il "for" successivo*/
+					{	
+						if(j != i)
 						{
-							*(p+i) = *(p+j);
-							*(q+i) = *(q+j);
-							break;
+							if(*(A+(i*COLUMN)+j) == 1)
+								tmp++;
+							if(tmp == pos)
+							{
+								*(p+i) = *(p+j);
+								*(q+i) = *(q+j);
+								tmp = -1;
+							}
 						}
 					}
 				}
