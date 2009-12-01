a7489317ca154f9b8fce2b1314478784f77c7974
mrblasco <mrblasco@05936896-c536-11de-ba9a-9db95b2bc6c5>
2009-12-01 18:07:26 +0000

    Ho fatto un po di modifiche... su matrices

--- src/elanja.c
+++ src/elanja.c
@@ -9,7 +9,9 @@ int main()
         int i, j = 0;
 	double k,v;
 	double Bp_average,Bq_average;
-	double alfa;			/*Measure of the non-segregation of the system*/
+	double alfa;
+	double beta;
+
 
 	double *p = malloc(sizeof(double)*COLUMN);
 	double *q = malloc(sizeof(double)*COLUMN);
@@ -23,18 +25,22 @@ int main()
 	int *B_degree = malloc(sizeof(int)*ROW);
 
 	/* Text file for printing the matrices */
-	FILE *out;	
+	FILE *out;
+	FILE *out2;	
 	FILE *out3;
+	FILE *out4;	
+	FILE *out5;
 	out = fopen("matrix.dat", "w");	
-	/*out2 = fopen("pq.dat", "w");*/
+	out2 = fopen("single_node.dat", "w");
 	out3 = fopen("alfa.dat", "w");
-
+	out4 = fopen("degree.dat", "w");
+	out5 = fopen("composition.dat", "w");
 
 	/* Initialize meeting probability vectors */
 	for(i=0; i<COLUMN; i++)	
 	{
-		p[i] = 0.5; 
-		q[i] = 0.5;
+		p[i] = 0.01; 
+		q[i] = 0.01;
 	}
 
 	Bp_average = 0;
@@ -44,43 +50,52 @@ int main()
  	for(i=0; i<NITER; i++)
 	{
 		interaction(p, q, A, R, degree, A_degree, B_degree);
+		/* multiply(A, A, B); */
 		update(p, q, degree, A_degree, B_degree);
 		externalUpdate(A, p, q, EPSILON, degree);
+		for(j=0; j<COLUMN; j++)
+		{ 
+			fprintf(out4,"%d\t",*(degree+j));
+			fprintf(out5,"%f\t", (double) *(A_degree+j) / *(degree +j));
+		}
+		fprintf(out4,"\n");
+		fprintf(out5,"\n");
 
-		for(j=0; j<COLUMN/2; j++)
+		
+		for(j=M/2; j<COLUMN; j++)
 		{ 
 			Bp_average += p[j];				
 			Bq_average += q[j];
 		}
+
 		
-		Bp_average = (double) Bp_average / (double) COLUMN; 
-		Bq_average = (double) Bq_average / (double) COLUMN; 
+		Bp_average = (double) Bp_average / COLUMN; 
+		Bq_average = (double) Bq_average / COLUMN; 
 		printf("p-Average %f\t", Bp_average);
 		printf("q-Average %f\t", Bq_average);
 		printf("\n");
 
 		/* Computation of alfa at each time step */ 
 		alfa = 0;
-		for(j=0; j<(COLUMN/2); j++)
+		beta = 0;
+		for(j=0; j<COLUMN; j++)
 		{
-			if(*(B_degree+j)>=T)
-				alfa++;
-		}
-		for(j=COLUMN/2; j<COLUMN; j++)
-		{	
-			if(*(A_degree+j)>=T)
+			if(*(B_degree+j)==0)
+				beta++;
+
+			if(*(A_degree+j)==0)
 				alfa++;
 		}
 
-		/*alfa = alfa / ROW;	*/
+/* Single node Behavior */
+		fprintf(out2, "%d\t", *(A_degree + COLUMN/2));
+		fprintf(out2, "%d\n", *(A_degree));
+/* Integration measure */
 		fprintf(out3, "%f\t", alfa);
-		
+		fprintf(out3, "%f\n", beta);		
 	} 
-	/* Adjacency matrix of nodes at distance d <= 2 */
-	multiply(A, A, B);
 
-
-	/*	printf("Extragroup Meeting Probability %f\n", (double) v / (double) COLUMN);*/
+	/*	printf("Extragroup Meeting Probability %f\n", (double) v / (double) COLUMN);
 
 	for(i=0; i<COLUMN; i++)
 	{
@@ -88,14 +103,14 @@ int main()
 			k += 1.0;
 		if(q[i]==0)
 			v += 1.0;
-	}
+	} */
 
 
 	/*printf("Degree\n");
 	for(i=0; i<COLUMN; i++)
 	{
 		printf("%d\n", degree[i]);
-	} */
+	}*/
 
 	/* Print matrices */
 	/*fprintf(out, "Adjacency Matrix\n"); */
@@ -110,15 +125,6 @@ int main()
 
 	fprintf(out, "\n");
 
-	/*fprintf(out, "Random Matrix\n");
-	for(i=0; i<ROW; i++)
-	{
-		for(j=0; j<COLUMN; j++)
-		{
-			fprintf(out, "%f\t\t", R[i][j]);
-		}
-		fprintf(out, "\n");
-	}*/
 return COLUMN;
 }
 
