a26330ea7f221dd012aba166107b10b91bc6c785
zissex <zissex@05936896-c536-11de-ba9a-9db95b2bc6c5>
2009-12-04 09:48:56 +0000

    Predisposizione per le distanze minori uguali di 2.

--- trunk/h/matrices.h
+++ trunk/h/matrices.h
@@ -4,8 +4,8 @@ extern void interaction(double *p, double *q, int *A, double *R, int *degree, in
 /* Update function */
 extern void update(double *p, double *q, int *degree, int *A_degree, int *B_degree);
 
-/*External Update */
+/* External Update */
 extern void externalUpdate(int *A, double *p, double *q, double epsilon, int *degree);
 
-/* multiply function */
-extern void distance(int *A, int *B, int *C, int *degree, int *A_degree, int *B_degree);
+/* Multiply function */
+extern void distance(int *A, int *B, int *C, int *degree, int *A_degree, int *B_degree, int d);
--- trunk/src/multiply.c
+++ trunk/src/multiply.c
@@ -3,21 +3,24 @@
 #include "../h/const.h"
 
 /* Function to compute Adjacency Matrices at d<=2 */
-void distance(int *A, int *B, int *C, int *degree, int *A_degree, int *B_degree) {
+void distance(int *A, int *B, int *C, int *degree, int *A_degree, int *B_degree, int d) {
 
-int i, j, l = 0;
+	int i, j, l;
 
-/*  Initialize AM */
-	for(i=0; i<ROW; i++) 
+	if(d==2)
+	{
+
+		/*  Initialize AM */
+		for(i=0; i<ROW; i++) 
 		{
-		for(j=0; j<COLUMN; j++)
+			for(j=0; j<COLUMN; j++)
 			{ 
 				*(C + i*COLUMN + j) = 0;
 			}
 		}
 
-/* Product */
-	for(i=0; i<ROW; i++)
+		/* Product */
+		for(i=0; i<ROW; i++)
 		{	
 			for(j=0; j<COLUMN; j++)
 			{		
@@ -25,8 +28,8 @@ int i, j, l = 0;
 					*(C + i*COLUMN +j) += *(A + i*COLUMN +l) * *(B + j*COLUMN +l); 
 			}	
 		} 
-/* Normalize Adjacency matrix adding distance one*/
-	for(i=0; i<ROW; i++)
+		/* Normalize Adjacency matrix adding distance one*/
+		for(i=0; i<ROW; i++)
 		{	
 			for(j=0; j<COLUMN; j++)
 			{	
@@ -36,51 +39,62 @@ int i, j, l = 0;
 			}      
 		} 
 
-/* Initialize degree, A_degree, B_degree vectors to 0 at each iteraction */
-	for(i=0; i<COLUMN; i++)
-	{
-		*(degree+i) = 0;
-		*(A_degree+i) = 0;
-		*(B_degree+i) = 0;
+		/* Initialize degree, A_degree, B_degree vectors to 0 at each iteraction */
+		for(i=0; i<COLUMN; i++)
+		{
+			*(degree+i) = 0;
+			*(A_degree+i) = 0;
+			*(B_degree+i) = 0;
+		}
+
+		/* Degree vector*/ 
+		for(i=0; i<ROW; i++)
+			{
+				for(j=0; j<COLUMN; j++)
+				{
+					*(degree+i) += *(C+(i*COLUMN)+j);
+			   	}
+			}
+
+		/* Degree of A-type agents vector*/ 
+		for(i=0; i<ROW; i++)
+			{
+				for(j=0; j<M; j++)
+				{
+					*(A_degree+i) += *(C+(i*COLUMN)+j);
+			   	}
+				for(j=M; j<COLUMN; j++)
+				{
+					*(B_degree+i) += *(C+(i*COLUMN)+j);
+			   	}
+			}
+
 	}
-
-/* Degree vector*/ 
-	for(i=0; i<ROW; i++)
-		{
-			for(j=0; j<COLUMN; j++)
+	else if (d==1)
+	{
+		/* Degree vector*/ 
+		for(i=0; i<ROW; i++)
 			{
-				*(degree+i) += *(C+(i*COLUMN)+j);
-		   	}
-		}
+				for(j=0; j<COLUMN; j++)
+				{
+					*(degree+i) += *(A+(i*COLUMN)+j);
+			   	}
+			}
 
-/* Degree of A-type agents vector*/ 
-	for(i=0; i<ROW; i++)
-		{
-			for(j=0; j<M; j++)
+		/* Degree of A-type agents vector*/ 
+		for(i=0; i<ROW; i++)
 			{
-				*(A_degree+i) += *(C+(i*COLUMN)+j);
-		   	}
-			for(j=M; j<COLUMN; j++)
-			{
-				*(B_degree+i) += *(C+(i*COLUMN)+j);
-		   	}
-		}
-
+				for(j=0; j<M; j++)
+				{
+					*(A_degree+i) += *(A+(i*COLUMN)+j);
+			   	}
+				for(j=M; j<COLUMN; j++)
+				{
+					*(B_degree+i) += *(A+(i*COLUMN)+j);
+			   	}
+			}
+	}
+	else
+		printf("Distance error d = %d\n", d);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
