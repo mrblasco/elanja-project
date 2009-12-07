ec6a5097e24333e32d154b10d1ef24c9e7fcde5d
zissex <zissex@05936896-c536-11de-ba9a-9db95b2bc6c5>
2009-12-07 16:37:37 +0000

    Aggiunta la generazione dei numeri casuali diversi ogni volta che si esegue il programma.

--- trunk/src/test.c
+++ trunk/src/test.c
@@ -1,35 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "../h/const.h"
 #include "../h/util.h"
 
 int main(){
 
-	printf("arrivo qua!!!");
+	
+	int i;
 
-        int i, j;
-	int p[COLUMN];
-	int q[COLUMN];
-        int R[ROW][COLUMN];
-        int A[ROW][COLUMN];
-	int degree[ROW];
-	int A_degree[ROW], B_degree[ROW];
+	srand(time(NULL));
 
-	/* Text file for printing the matrices */
-	FILE *out;	
-	out = fopen("prova.txt", "w");
-
-	printf("arrivo qua!!!");	
-
-	intraction(p, q, *A, *R, degree, A_degree, B_degree);
-
-	for(i=0; i<ROW; i++)
+	for(i=0; i<4; i++)
 	{
-		for(j=0; j<COLUMN; j++)
-		{
-			fprintf(out, "%d\t\t", A[i][j]);
-		}
-		fprintf(out, "\n");
+		printf("Numero casuale: %d\n", rand()%10);
 	}
 
 	return 0;
