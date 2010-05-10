b6015100138ba6cfac5b2dc91edc31d31284fb8d
mrblasco <mrblasco@05936896-c536-11de-ba9a-9db95b2bc6c5>
2010-05-10 10:27:41 +0000

    Modello funziona!

--- elanja-fltk/model.h
+++ elanja-fltk/model.h
@@ -11,14 +11,15 @@
 
 class model{
 public: 
-	void init(int agents, int distance, double population, double epsilon, double friendship); /* inizializza il modello */
-	void reinit(int agents, int distance, double population, double epsilon, double friendship); /* re-inizializza  */
-	void step(); /* one-step  */
+     /* initialize functions and variables */
+	void init(int agents, int distance, double Agroup, double epsilon, double friendship); 
+	void reinit(int agents, int distance, double Agroup, double epsilon, double friendship); 
+	void step(); 
 	
 	int agents;
 	int distance;
 	int m;
-	double population;
+	double Agroup;
 	double epsilon;
 	double friendship;
 
@@ -35,14 +36,13 @@ public:
 
 	double *composition;
 	
-	
 };
 
 void interaction(int agents, int m,  double *p, double *q, int *A, double epsilon, double friendship);
 
 void multiplyer(int agents, int *A, int *C);
 
-void update(int agents, double *p, double *q, int *degree, int *A_degree, int *B_degree);
+/*void update(int agents, double *p, double *q, int *degree, int *A_degree, int *B_degree);
 void externalUpdate(int agents, int *A, double *p, double *q, double epsilon, int *degree);
-
+*/
 #endif
--- elanja-fltk/simulationGrid.cpp
+++ elanja-fltk/simulationGrid.cpp
@@ -1,30 +1,32 @@
 #include "simulationGrid.h"
 #include "const.h"
 
-extern int gui_agents; /* graphical user interface ... gui => agenti modificati dalla barra */
-extern int gui_distance;
-extern double gui_population;
-extern double gui_epsilon;
+/* Graphical User Interface */
+extern int gui_agents;  
+extern int gui_distance; 
+extern double gui_Agroup; /* fraction of a-type people over population*/
+extern double gui_epsilon; /* inherited fracion of links  */
+extern double gui_friendship; /*  fraction of population met in one iteration */
 
-extern double gui_friendship;
-extern bool restart;
+extern bool restart; 
 extern double simSpeed;
-extern model m; /* chiami clase model -> m */
+extern model m;   /* object of class 'model' is called 'm' */
 
 bool initModel=true;
 int W,H;
 
-simulationGrid::simulationGrid(int x,int y,int w,int h, const char *l):Fl_Gl_Window(x,y,w,h,l)  /*invoca costruttore FLGLWINDOW*/
+/*invoca costruttore FLGLWINDOW*/
+simulationGrid::simulationGrid(int x,int y,int w,int h, const char *l):Fl_Gl_Window(x,y,w,h,l)  
 {
 /*	grow = true; */
-	restart = true; 
+	restart = false; 
 	W=w;
 	H=h;
 }
 
 void simulationGrid::init(){ 
      /* initializza la simulation grid */ 
-	valid(1); /*valido?*/
+	valid(1); /*valido <=> grid inizializzata*/
 	glLoadIdentity(); 
 	glEnable(GL_BLEND);
 	glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -36,21 +38,20 @@ void simulationGrid::init(){
 	/* parametri iniziali */
 	gui_agents = AGENTS_INIT;  
 	gui_distance = DISTANCE_INIT;
-	gui_population = (AGENTS_INIT / 2);
+	gui_Agroup = AGROUP_INIT;
 	gui_epsilon = EPSILON_INIT;
 	gui_friendship = SAMPLE_INIT;
-	
 	simSpeed = SIM_SPEED_INIT;
 	
 if(initModel){ 
-		m.init(AGENTS_INIT, DISTANCE_INIT, FRACTION_INIT, EPSILON_INIT, SAMPLE_INIT);  /* chiama la classe model , metodo init()*/ 
+		m.init(AGENTS_INIT, DISTANCE_INIT, AGROUP_INIT , EPSILON_INIT, SAMPLE_INIT);   
 		initModel=false; 
 	}else{		
 		restart = true;
 	}
 }
 
-void simulationGrid::draw() {	 /*metodo draw ... per disegnare le palline*/
+void simulationGrid::draw() {	
 	int i, j;
 	
 	/*inizializzazione grafica */
@@ -58,13 +59,13 @@ void simulationGrid::draw() {	 /*metodo draw ... per disegnare le palline*/
 		init(); 
 	
    if (restart)
-		m.reinit(gui_agents,gui_distance, gui_population, gui_epsilon, gui_friendship);   
+		m.reinit(gui_agents,gui_distance, gui_Agroup, gui_epsilon, gui_friendship);   
 	
 	/* passa al modello i parametri presi dalla gui  e reiinit  */
-	if(m.agents != gui_agents || m.population != gui_population || m.epsilon != gui_epsilon)
-		m.reinit(gui_agents,gui_distance, gui_population, gui_epsilon, gui_friendship); 
+	if(m.agents != gui_agents || m.Agroup != gui_Agroup || m.epsilon != gui_epsilon)
+     	m.reinit(gui_agents,gui_distance, gui_Agroup, gui_epsilon, gui_friendship);         
 
-	/*m.agents = gui_agents;
+	/* m.agents = gui_agents;
 	m.population = gui_population;
 	m.epsilon = gui_epsilon;*/
 
@@ -78,17 +79,18 @@ void simulationGrid::draw() {	 /*metodo draw ... per disegnare le palline*/
 	{		
 		drawAgents(i);	
 	}
+     restart=false;
 }
 
 void drawAgents(int i){
 	int k, j;		 	
 
-	if ( i < m.m ) /*modello. m = perc tipo ... */
+	if ( i < m.m ) 
 	{
           /* rosso, green, blue, opacity */
 		glColor4d(1 - m.composition[i], 0, m.composition[i],0.7);
           /* position_x, position_ y, size */
-		//circle(rand() % (745/2 - 10) +10, rand() % (490) + 10, 3.0 * (double) sqrt(m.degree[i]));
+		circle(rand() % (745/2 - 10) +10, rand() % (490) + 10, 3.0 * (double) sqrt(m.degree[i]));
 	}
 	if (i>=m.m)
 	{
@@ -96,8 +98,6 @@ void drawAgents(int i){
 		glColor4f(m.composition[i],0,(1-m.composition[i]),0.7);
 		circle((745/2 +10) + (rand() % (745/2 - 20)), rand() % (490) + 10, 3.0 * (double) sqrt(m.degree[i]));
 	}
-
-	restart = false;
 }
 
 void timer_cb(void *p) /*delay tra step e altro*/
