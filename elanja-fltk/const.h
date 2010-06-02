ce3a234709448ff16e8221d1973f3cbcda65beb2
mrblasco <mrblasco@05936896-c536-11de-ba9a-9db95b2bc6c5>
2010-06-02 14:10:04 +0000

    migliora visualizzazione but still missing "links and network"

--- elanja-fltk/const.h
+++ elanja-fltk/const.h
@@ -1,5 +1,5 @@
-#define MAX_AGENTS			100
-#define AGENTS_INIT		    100
+#define AGENTS_MAX			400
+#define AGENTS_INIT		    200
 #define DISTANCE_INIT         1 
 #define EPSILON_INIT		0.5  /*  mantiene vecchi links */
 #define SAMPLE_INIT			1.0  /* 1 = tutto il netw, 0 uguole nessuno  */
