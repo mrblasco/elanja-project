fe9ce9d61d83f30b0bd25ca7683652ba153dd0cb
mrblasco <mrblasco@05936896-c536-11de-ba9a-9db95b2bc6c5>
2010-06-11 06:56:12 +0000

    Links II -

--- elanja-fltk/const.h
+++ elanja-fltk/const.h
@@ -1,12 +1,12 @@
 #define AGENTS_MAX			1000
-#define AGENTS_INIT		    100
+#define AGENTS_INIT		    200
 #define DISTANCE_INIT         1 
 #define FEATURES_INIT		3.0  /*  mantiene vecchi links */
 #define SAMPLE_INIT			1.0  /* 1 = tutto il netw, 0 uguole nessuno  */
 
 #define FRIEND_INIT		0.5  
 #define FRACTION_INIT		0.5  
-#define RHO_INIT		0.2  
+#define RHO_INIT		0.05  
 #define SIM_SPEED_INIT		0.32 /* simulation speed */
 #define OUTDEGREE_INIT		10
 //#define NFEATURES_INIT		3
