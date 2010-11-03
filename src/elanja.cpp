078a8b0bb84bb16a0573e4aec64a5055234190b6
elisa.omodei <elisa.omodei@05936896-c536-11de-ba9a-9db95b2bc6c5>
2010-11-03 11:43:33 +0000

    Ho integrato la parte testuale con il codice scritto da Andrea per la creazione del network di partenza

--- src/elanja.cpp
+++ src/elanja.cpp
@@ -32,14 +32,17 @@ int main(int argc, char **argv)
 		printf("	- features %d\n",NFEATURES);
 		printf("	- number of possible values for any feature %d\n",POS_FEATURES);
 		printf("	- number of iterations %d\n", N_ITER);
+		printf("	- degree %d\n", OUTDEGREE);
+		printf("	- delta %f\n", DELTA);
+		printf("	- maxSide %d\n", MAXSIDE);
 
 		/* Set model parameters */
-		m.init(EDGE_AGENTS, AGENTS, NFEATURES, POS_FEATURES, N_ITER);
+		m.init(EDGE_AGENTS, AGENTS, NFEATURES, POS_FEATURES, N_ITER, OUTDEGREE, DELTA, MAXSIDE);
 
-	} else if (argc == 5) {
+	} else if (argc == 8) {
 
 		/* Set model parameters */
-		m.init(atoi(argv[1]),atoi(argv[1])*atoi(argv[1]),atoi(argv[2]),atoi(argv[3]),atoi(argv[4]));
+		m.init(atoi(argv[1]),atoi(argv[1])*atoi(argv[1]),atoi(argv[2]),atoi(argv[3]),atoi(argv[4]),atoi(argv[5]),atoi(argv[6]),atoi(argv[7]));
 
 		printf("Using parameters: \n");
 		printf("	- linear edge agents %d\n", m.edge_agents);
@@ -47,10 +50,14 @@ int main(int argc, char **argv)
 		printf("	- features %d\n", m.nFeatures);
 		printf("	- number of possible values for any feature %d\n", m.pos_features);
 		printf("	- number of iterations %d\n", m.n_iter);
+		printf("	- degree %d\n", m.outdegree);
+		printf("	- delta %f\n", m.delta);
+		printf("	- maxSide %d\n", m.maxSide);
+
 	} 	else 	{
 
           printf(">>>>>>>>>>>>>>>>>>>>>>>>> E R R O R <<<<<<<<<<<<<<<<<<<<<<<<<<<<< \n");
-		printf("	- usage: ./main <n LINEAR EDGE AGENTS> <n NUMBER of FEATURES> <n POSSIBLE FEATURES> <n NUMBER OF ITERACTION>\n");
+		printf("	- usage: ./main <n LINEAR EDGE AGENTS> <n NUMBER of FEATURES> <n POSSIBLE FEATURES> <n NUMBER OF ITERACTION> <DEGREE> <DELTA> <MAXSIDE>\n");
 		exit(0);
 	}
 
