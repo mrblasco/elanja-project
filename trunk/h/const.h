901acdfac5968ae7416a732f68054540fae1b767
elisa.omodei <elisa.omodei@05936896-c536-11de-ba9a-9db95b2bc6c5>
2010-11-23 09:33:22 +0000

    La dinamica si ferma se il sistema è frozen o se t > 100*agents

--- trunk/h/const.h
+++ trunk/h/const.h
@@ -1,10 +1,10 @@
 #define EDGE_AGENTS 20
 #define AGENTS EDGE_AGENTS*EDGE_AGENTS
 #define NFEATURES 3
-#define POS_FEATURES 20
+#define POS_FEATURES 10
 #define N_ITER 10
 #define OUTDEGREE 4
-#define DELTA 10.0
+#define DELTA 0.0
 #define MAXSIDE 0
 
 
