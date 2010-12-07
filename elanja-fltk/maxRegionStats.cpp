6a87600bc6cf7ab77e370903ed64fff091b7ee00
zissex <zissex@05936896-c536-11de-ba9a-9db95b2bc6c5>
2010-12-07 14:23:22 +0000

    Come prima!!!

---
+++ elanja-fltk/maxRegionStats.cpp
@@ -0,0 +1,59 @@
+#include "maxRegionStats.h"
+#include "simulationGrid.h"
+
+extern model m;
+extern bool doNextSimulationStep;
+
+maxRegionStats::maxRegionStats(int w, int h, const char *l):glStats(w,h,l) 
+{
+	int i,j;
+
+     for(i=0;i<5;i++)
+          for(j=0;j<20;j++)  
+               degreeHistory[i][j] = 0.0;
+}
+
+
+void maxRegionStats::paint() 
+{	
+	int i,j;
+     double vector[20];
+
+
+     for (i=0;i<m.agents;i++)
+     {
+          vector[m.degree[i]]++;
+     }
+     /* aggiorna history */
+     for(i=0;i<5;i++)
+     {
+          for(j=0;j<20;j++)
+          {  
+               degreeHistory[i][j] = degreeHistory[(i+1)][j];
+          }
+     }
+     for(j=0;j<20;j++)
+     {  
+          vector[j] = vector[j] / m.agents;
+          degreeHistory[4][j] = vector[j];
+     }
+
+	if(doNextSimulationStep){
+		glClear(GL_COLOR_BUFFER_BIT);
+		
+		for (i=0; i<5; i++)
+		{
+			     for (j=0; j<20; j++)
+			     {
+			glColor4f(0.0,1.0,1.0,0.5);
+	     			glBegin(GL_POLYGON);
+				     glVertex2d(70*i, (STATS_H-10)  -j*20);
+				     glVertex2d(70*(i+degreeHistory[i][19-j]), (STATS_H-10)  -j*20);
+				     glVertex2d(70*(i+degreeHistory[i][19-j]), (STATS_H-10)- (j+1)*20 );
+				     glVertex2d(70*i, (STATS_H-10) - (j+1)*20);
+
+				glEnd();
+	          	}
+		}
+	}
+}
---
+++ elanja-fltk/regionCountStats.h
@@ -0,0 +1,21 @@
+#ifndef GRAPHIC_2_H
+#define GRAPHIC_2_H
+
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "glStats.h"
+#include "model.h"
+
+class regionCountStats : public glStats
+{
+public:
+	regionCountStats(int w,int h,const char *l=0);
+     double clusteringHistory[STATS_W];	
+     double clustAvgHistory[STATS_W];	
+
+protected:
+	void paint();
+};
+
+#endif
